c94d36af3217a1388cf56a5d3b857833b5ebe17b
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-01-27 19:18:19 +0530

    Create Shuffle the Array.cpp

---
+++ Shuffle the Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>v1;
+        vector<int>v2;
+        vector<int>vf;
+        for(int i=0; i<n; i++)
+            v1.push_back(nums[i]);
+        for(int i=n; i<2*n; i++)
+            v2.push_back(nums[i]);
+        
+        for(int i=0; i<n; i++)
+        {
+            vf.push_back(v1[i]);
+            vf.push_back(v2[i]);
+        }
+        return vf;
+        
+    }
+};
