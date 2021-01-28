91d01dd310efcbd8ad82e1a5d26f55d62c6fa580
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-01-29 00:30:39 +0530

    Update Goal Parser Interpretation.cpp

--- Goal Parser Interpretation.cpp
+++ Goal Parser Interpretation.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     string interpret(string command) {
         string s="", change="o";
@@ -6,12 +6,28 @@ public:
         {
             if(command[i]=='(' && command[i+1]==')')
                 s+= change;
-            if(command[i]=='(' ||command[i]==')')
+            if(command[i]=='(' || command[i]==')')
                 s+="";
             else
                 s += command[i];
         }
         return s;
         
+    }
+};*/
+
+class Solution {
+public:
+    string interpret(string command) {
+        string s="";
+       for(int i=0; i<command.size();)
+       {
+           if(command[i]=='G')
+               s+="G", i+=1;
+           else if(command[i]=='(' && command[i+1]==')') s+="o", i+=2;
+           else s+= "al", i+=4;
+               
+       }
+        return s;
     }
 };
