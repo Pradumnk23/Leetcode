b48e02377d80db35a12c08ec6eb62a59033da38c
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-01-31 21:03:23 +0530

    Create XOR Operation in an Array.cpp

---
+++ XOR Operation in an Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int>v1;
+        for(int i=0; i<n; i++)
+            v1.push_back(start+2*i);
+        int ans = v1[0];
+        for(int i=1; i<v1.size(); i++)
+            ans ^= v1[i];
+        return ans;
+    }
+};
+/* ------BETTER APPROACH------
+
+int xorOperation(int n, int start) {   
+		int res = start;
+		for (int i=1; i<n; i++){
+			res = res ^ (start + 2 * i);
+			}
+		return res;
+	}
+*/
