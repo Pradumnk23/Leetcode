bf59efe66e3358c178f50d97c96c2c4d36086b80
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-02-02 23:46:23 +0530

    Create Count the Number of Consistent Strings.cpp

---
+++ Count the Number of Consistent Strings.cpp
@@ -0,0 +1,24 @@
+/* ---------------------- IMP ----------------------
+------------ NEED TO LEARN ---------------- */
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int cnt = words.size();
+        bool alpha[123]={};
+        for(auto c:allowed) alpha[c]=true;
+        for(auto w:words)
+        {
+            for(auto c:w)
+            {
+                if(!alpha[c])
+                {
+                    cnt--;
+                    break;
+                }
+            }
+        }
+        return cnt;
+        
+    }
+};
