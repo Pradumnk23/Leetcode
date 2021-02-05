3fe15f7c2cef9c5c16fa295ea014cdfada36e588
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-02-05 20:03:28 +0530

    Update Count of Matches in Tournament.cpp

--- Count of Matches in Tournament.cpp
+++ Count of Matches in Tournament.cpp
@@ -4,3 +4,14 @@ public:
         return n-1;
     }
 };
+
+/*
+int numberOfMatches(int n) {
+    int cnt = 0;
+    while (n > 1) {
+        cnt += n / 2;
+        n = (n + 1) / 2;
+    }
+    return cnt;
+}
+*/
