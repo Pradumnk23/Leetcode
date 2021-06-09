2a853e0d3c3747d864feda2342495a79d2d06a58
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-06-09 13:00:56 +0530

    sort

---
+++ Valid Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string a, string b) {
+        
+        if(a.length()!=b.length())
+            return false;
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        for(int i=0; i<a.length(); i++)
+        {
+            if(a[i]!=b[i])
+                return false;
+        }
+        return true;
+    }
+};
