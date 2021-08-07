1d707452fbb8e3266751b8372f8d0c90a8ca6422
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-08-07 16:30:10 +0530

    .

--- Binary Tree Right Side View.cpp
+++ Binary Tree Right Side View.cpp
@@ -8,7 +8,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+  */
 class Solution {
 public:
     void recursion(TreeNode *root, int level, vector<int> &res)
